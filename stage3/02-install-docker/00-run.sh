19ad9c995c122fc8588ed1cf95b23332c35c3d77
Matt Robinson <brimstone@the.narro.ws>
2017-08-07 20:34:30 -0400

    Remove systemd file and pin docker engine version

--- stage3/02-install-docker/00-run.sh
+++ stage3/02-install-docker/00-run.sh
@@ -2,12 +2,10 @@
 
 install -m 644 files/docker.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
-install -m 644 files/docker.service "${ROOTFS_DIR}/etc/systemd/system/docker.service"
-
 on_chroot apt-key add - < files/docker.gpg.key
 on_chroot << EOF
 apt-get update
-apt-get install -y docker-engine
+apt-get install -y docker-ce=17.07.0~ce~rc1-0~raspbian
 EOF
 
 on_chroot  << EOF
